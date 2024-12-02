dd857529846619536e1f839c1702aab954fc9eab
AlejandroGonzalez0 <alejandro.gonzales@aragoconsulting.com>
2024-12-02 11:49:42 -0500

    -> Se elimina la minima partición. -> Se termina la primera estrategia -> Se empieza la segunda estrategia -> Se empieza la tercera estrategia

--- backend/index.py
+++ backend/index.py
@@ -13,7 +13,10 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from backend.strategies.bruteForce import BruteForce
-from backend.strategies.minimaParticion import decomposition
+from backend.strategies.FirstStrategy import FirstStrategy
+from backend.strategies.SecondStrategy import SecondStrategy
+from backend.strategies.ThirdStrategy import ThirdStrategy
+
 from backend.generator.probabilities import generatorProbabilities
 from backend.candidateSystemGenerator.candidateGenerator import indexCandidateSystem, marginalize_variableFuture, marginalize_variablePresent
 
@@ -160,7 +163,91 @@ if data is not None:
 
             st.caption("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec dignissim nulla. Proin porta nulla eros, ac posuere nisi molestie et. Nulla dapibus pellentesque enim, at elementum nulla mollis ut. Nunc convallis ultricies augue faucibus sagittis. Mauris hendrerit lorem a nunc porta dignissim. Sed vehicula.")
 
-            mejor_particion, min_emd = decomposition(sysC_nextStatus, sysC_currentStatus, dataJson["stateSought"], result_matrix, states, st)
+            firstStrategy = FirstStrategy(result_matrix, dataJson["stateSought"], states, sysC_currentStatus, sysC_nextStatus)
+            mejor_particion, min_emd = firstStrategy.strategy()
 
-            st.text(mejor_particion)
-            st.text(min_emd)
+    with st.expander("Segunda Estrategia"):
+
+        st2_candidateSystem_Perfect = []
+
+        st.divider()
+
+        st.title("¿Desea Crear un sistema Candidato?")
+
+        st.write("Llené los siguientes datos, teniendo en cuenta que solo se puede marginalizar variables que se encuentren continuas")
+        st2_candidateSystem = st.text_input("Sistema candidato - Estrategia 2", "ABC")
+
+        st2_execCandidateSystem = st.button("Obtener sistema candidato - Estrategia 2")
+
+        if st2_execCandidateSystem:
+
+            st2_candidateSystem_Imperfect = indexCandidateSystem(result_matrix,st2_candidateSystem, varData)
+            st2_candidateSystem_Perfect = marginalize_variableFuture(st2_candidateSystem_Imperfect,st2_candidateSystem, varData)
+            
+            st.subheader("Tabla de Sistema Candidato - Imperfecta")
+            st.table(st2_candidateSystem_Imperfect)
+
+            st.subheader("Tabla de Sistema Candidato - Perfecta (Marginalizada)")
+            st.table(st2_candidateSystem_Perfect)
+
+            st.divider()
+
+        st.subheader("Subsistema")
+        
+        st.caption("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec dignissim nulla. Proin porta nulla eros, ac posuere nisi molestie et. Nulla dapibus pellentesque enim, at elementum nulla mollis ut. Nunc convallis ultricies augue faucibus sagittis. Mauris hendrerit lorem a nunc porta dignissim. Sed vehicula.")
+
+        st2_sysC_currentStatus = st.text_input("Estado Presente - Estrategia 2", "")
+        st2_sysC_nextStatus = st.text_input("Estado Futuro - Estrategia 2", "")
+
+        st2_execPairContruction = st.button("Obtener Pares - Estrategia 2")
+
+        if st2_execPairContruction:
+
+            st.caption("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec dignissim nulla. Proin porta nulla eros, ac posuere nisi molestie et. Nulla dapibus pellentesque enim, at elementum nulla mollis ut. Nunc convallis ultricies augue faucibus sagittis. Mauris hendrerit lorem a nunc porta dignissim. Sed vehicula.")
+
+            secondtStrategy = SecondStrategy(result_matrix, dataJson["stateSought"], states, sysC_currentStatus, sysC_nextStatus)
+            mejor_particion, min_emd = secondtStrategy.strategy()
+
+    with st.expander("Tercera Estrategia"):
+
+        st3_candidateSystem_Perfect = []
+
+        st.divider()
+
+        st.title("¿Desea Crear un sistema Candidato?")
+
+        st.write("Llené los siguientes datos, teniendo en cuenta que solo se puede marginalizar variables que se encuentren continuas")
+        st3_candidateSystem = st.text_input("Sistema candidato - Estrategia 3", "ABC")
+
+        st3_execCandidateSystem = st.button("Obtener sistema candidato - Estrategia 3")
+
+        if st3_execCandidateSystem:
+
+            st3_candidateSystem_Imperfect = indexCandidateSystem(result_matrix,st3_candidateSystem, varData)
+            st3_candidateSystem_Perfect = marginalize_variableFuture(st3_candidateSystem_Imperfect,st3_candidateSystem, varData)
+            
+            st.subheader("Tabla de Sistema Candidato - Imperfecta")
+            st.table(st3_candidateSystem_Imperfect)
+
+            st.subheader("Tabla de Sistema Candidato - Perfecta (Marginalizada)")
+            st.table(st3_candidateSystem_Perfect)
+
+            st.divider()
+
+        st.subheader("Subsistema")
+        
+        st.caption("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec dignissim nulla. Proin porta nulla eros, ac posuere nisi molestie et. Nulla dapibus pellentesque enim, at elementum nulla mollis ut. Nunc convallis ultricies augue faucibus sagittis. Mauris hendrerit lorem a nunc porta dignissim. Sed vehicula.")
+
+        st3_sysC_currentStatus = st.text_input("Estado Presente - Estrategia 3", "")
+        st3_sysC_nextStatus = st.text_input("Estado Futuro - Estrategia 3", "")
+
+        st3_execPairContruction = st.button("Obtener Pares - Estrategia 3")
+
+        if st3_execPairContruction:
+
+            st.caption("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec dignissim nulla. Proin porta nulla eros, ac posuere nisi molestie et. Nulla dapibus pellentesque enim, at elementum nulla mollis ut. Nunc convallis ultricies augue faucibus sagittis. Mauris hendrerit lorem a nunc porta dignissim. Sed vehicula.")
+
+            thirdStrategy = ThirdStrategy(result_matrix, dataJson["stateSought"], states, sysC_currentStatus, sysC_nextStatus)
+            mejor_particion, min_emd = thirdStrategy.strategy()
+
+    
